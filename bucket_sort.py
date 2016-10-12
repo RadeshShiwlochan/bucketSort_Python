cb57566efc702c4b699a1060d25250d3a2090b06
radeshshiwlochan <radesh0430@gmail.com>
2016-10-11 23:39:23 -0400

    adds  own implementation of min and max

--- bucket_sort.py
+++ bucket_sort.py
@@ -16,5 +16,22 @@ def print_list(input_list):
     for num in input_list:
         print num + "  "
 
+def get_min(a_list):
+    index = 0
+    the_min = a_list[index]
+    for num in a_list:
+        if num < the_min:
+            the_min = num
+    return the_min
+
+def get_max(a_list):
+    the_max = 0
+    for num in a_list:
+        if num > the_max:
+            the_max = num
+    return the_max
+
 read_in_file('list_of_numbers.txt')
 print_list(the_list)
+min = get_min(the_list)
+print min
